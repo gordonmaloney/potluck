2f6447db78d6e591c285ceae5453e8480461ded1
Gordon Maloney <gordonmaloney@gmail.com>
2021-08-20 17:32:07 +0100

    render potlucks

--- src/components/yourpotlucks.js
+++ src/components/yourpotlucks.js
@@ -1,50 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardHeader, CardBody, CardFooter, Button } from "reactstrap";
 import { Control, LocalForm } from "react-redux-form";
+import { connect } from "react-redux";
+import CrazyTags from "./crazytags";
 
-export default function YourPotlucks() {
-  return (
-    <div>
-      <h2>Your potlucks</h2>
-
-      <Card>
-        <CardHeader>
-          <h1>Gordon's birthday potluck</h1>
-          <b>Date</b>: 10/08/1990
-          <br />
-          <b>Time</b>: 14:00
-          <br />
-          <b>Location</b>: Gordon's flat
-          <br />
-          <b>Theme:</b> Mexican
-          <br />
-          <b>Essentials:</b> Tortillas, salsa, guacamole
-        </CardHeader>
-        <CardBody>
-            <h4>What are you bringing?</h4>
-            <LocalForm>
-            <Control.text
-              model=".imbringing"
-              id="imbringing"
-              placeholder="I'm bringing..."
-              className="form-control"
-            />
-            <Button color="primary" className="mx-2 mt-3 float-end">
-              Confirm
-            </Button>
-            </LocalForm>
-        </CardBody>
-        <CardFooter>
-            <h5>Who's bringing what?</h5>
-            <ul>
-                <li>Mike Williamson: arroz rojo, chips and dip</li>
-                <li>Kirsty Haigh: pork carnitas</li>
-                <li>James McAsh: lettuce, cheese, sour cream</li>
-
-
-            </ul>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+function mapStateToProps(state) {
+  return {
+    potlucks: state.ActionsReduced.PotlucksReducer,
+    essentials: state.ActionsReduced.EssentialsReducer,
+  };
 }
+
+class YourPotlucks extends Component {
+  PotluckCard() {
+    console.log(this.props.potlucks);
+    return this.props.potlucks.map((potluck) => {
+      return (
+        <>
+          <Card className="mb-3">
+            <CardHeader>
+              <h3>{potluck.potluckTitle}</h3>
+              {potluck.potluckDate} <br />
+              <b>Theme: </b>
+              {potluck.potluckTheme} <br />
+              <b>Essentials:</b>
+              {potluck.essentials.map((essential, index) => {
+                return (
+                  <>
+                    {" "}
+                    {essential}
+                    {index !== potluck.essentials.length - 1 ? "," : ""}{" "}
+                  </>
+                );
+              })}
+            </CardHeader>
+            <CardBody>
+              <h4>What are you bringing?</h4>
+              <LocalForm>
+
+              <CrazyTags />
+
+                <Button color="primary" className="mx-2 mt-3 float-end">
+                  Confirm
+                </Button>
+              </LocalForm>
+            </CardBody>
+          </Card>
+        </>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Your potlucks</h2>
+        {this.PotluckCard()}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(YourPotlucks);
